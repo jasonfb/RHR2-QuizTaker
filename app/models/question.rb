06ce05b9a96f306b65767e6a27e406c68d31fa37
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-02-05 16:56:27 -0500

    implements question_spec for question belongs_to quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+
+  belongs_to :quiz
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {Question.new}
+  it {should belong_to(:quiz)}
 end
