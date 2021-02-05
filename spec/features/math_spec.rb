f888368a0432fa2f1466cb32c030a988aeeab840
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-02-05 15:35:33 -0500

    adds basic capy+jest setup

--- spec/features/math_spec.rb
+++ spec/features/math_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "take a quiz", type: :feature do
   it "asks me to add to two numbers" do
     visit '/'
-    expect(page).to have_content 'What is 5+2'
+    expect(page).to have_content 'Hello world'
   end
 end
